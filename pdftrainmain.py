b1d01d864992d8480c3d8c1f68cd48d1b842293e
Jeffrey Tom <jtom@GIRLINGNY.HHCSNY.COM>
2021-11-12 16:48:47 -0500

    jeffdev branch

--- pdftrainmain.py
+++ pdftrainmain.py
@@ -1,22 +1,18 @@
-
-
 # Converts multipage pdf to individual jpg files
-
-
-
-from pdf2image import convert_from_path
+from pdf2image import convert_from_path, convert_from_bytes
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
     PDFSyntaxError
 )
 print('starting process')
+
 #poppler_path = r'C:/Program Files/poppler/poppler-21.10.0/Library/bin'
 # Store Pdf with convert_from_path function
-images = convert_from_path("K:/Reporting/GirlingPDFs/All_Docs.pdf",single_file=False)
+images = convert_from_path("K:/Reporting/HCS TimeSheets/20211004-1.pdf",single_file=False, poppler_path=r'\\ghcnyfs01\users\JTom\Downloads\Release-21.11.0-0 (2)\poppler-21.11.0\Library\bin')
 #images = convert_from_path('K:/Reporting/PDFtoJPG/DOC100721-10072021170930 (1).pdf')
 for i in range(len(images)):
 
 # Save pages as images in the pdf
-    images[i].save('K:/Reporting/GirlingPDFs/jpgs/page'+ str(i) +'.jpg', 'JPEG')
+   images[i].save('K:/Reporting/HCS TimeSheets/Timesheetsjpgs/page'+ str(i) +'.jpg', 'JPEG')
 
