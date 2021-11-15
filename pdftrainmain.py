6a9ee448a825c1e892ebab305fcd63813345ab8f
Jeffrey Tom <jtom@GIRLINGNY.HHCSNY.COM>
2021-11-15 09:34:12 -0500

    added comments for poppler_path

--- pdftrainmain.py
+++ pdftrainmain.py
@@ -7,7 +7,11 @@ from pdf2image.exceptions import (
 )
 print('starting process')
 
+#Dom Dev Poppler Path
 #poppler_path = r'C:/Program Files/poppler/poppler-21.10.0/Library/bin'
+
+#Jeff Dev Poppler Path
+#poppler_path=r'\\ghcnyfs01\users\JTom\Downloads\Release-21.11.0-0 (2)\poppler-21.11.0\Library\bin'
 # Store Pdf with convert_from_path function
 images = convert_from_path("K:/Reporting/HCS TimeSheets/20211004-1.pdf",single_file=False, poppler_path=r'\\ghcnyfs01\users\JTom\Downloads\Release-21.11.0-0 (2)\poppler-21.11.0\Library\bin')
 #images = convert_from_path('K:/Reporting/PDFtoJPG/DOC100721-10072021170930 (1).pdf')
